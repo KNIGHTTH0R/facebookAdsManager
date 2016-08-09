248a28376e852bb35863e0016222b74bd44f161e
Jim Seconde <jim.seconde@googlemail.com>
2016-08-09 22:00:54 +0100

    removed token, get statics in

--- execute.php
+++ execute.php
@@ -3,8 +3,6 @@ use FacebookAds\Api;
 
 $app_id = 145634995501895;
 
-$access_token = 'EAACEdEose0cBAKr7YZAAEgBjML0h8eikPwredryIcvYD74Apa2VMHyZBVMXTWKPQ8lvD8wZB3EjocOPMyM4k20xfkoqf1jOkkZCQRCohNL3gaSc83PZB3rFLkMPTJcZAtZBSzfGrS6MejNcFdYaPsJqDgr25UqIGt72GxHRM4pJfHeZAWAXqPuUq';
-
 // Initialize a new Session and instanciate an Api object
 Api::init($app_id, $app_secret, $access_token);
 
